5f7f975241e1e1f3f10a6cd792253b8701e4c633
Sergei <sfccck@rambler.ru>
2022-08-09 15:01:05 +0300

    Задачку №6 решил

--- Lesson_01/Homework02_Lesson_01/Program.cs
+++ Lesson_01/Homework02_Lesson_01/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n == -3)
+{
+    Console.WriteLine("Нет");
+}
+
+if(n == -2)
+{
+    Console.WriteLine("да");
+}
+
+if(n == -1)
+{
+    Console.WriteLine("Нет");
+}
+
+
+if(n == 1)
+{
+    Console.WriteLine("Нет");
+}
+
+if(n == 2)
+{
+    Console.WriteLine("да");
+}
+
+if(n == 3)
+{
+    Console.WriteLine("Нет");
+}
+
+if(n == 4)
+{
+    Console.WriteLine("да");
+}
+
+if(n == 5)
+{
+    Console.WriteLine("Нет");
+}
+
+if(n == 6)
+{
+    Console.WriteLine("да");
+}
+
+if(n == 7)
+{
+    Console.WriteLine("Нет");
+}
