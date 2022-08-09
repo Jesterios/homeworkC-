ff7c01131cbb1ab00198d410ebd8e60784fbd0e5
Sergei <sfccck@rambler.ru>
2022-08-09 17:38:55 +0300

    Задачу №8 выполнил

--- Lesson_01/Homework03_Lesson_01/Program.cs
+++ Lesson_01/Homework03_Lesson_01/Program.cs
@@ -3,4 +3,25 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-int n = 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+class Program
+{
+static void Main(string[] args)
+{
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+if (i % 2 == 0) 
+{
+Console.Write(i + " ");
+}
+}
+Console.ReadKey();
+}
+}
