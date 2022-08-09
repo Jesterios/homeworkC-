41655d3f6ee4e30095a42ba15a431510217a397d
Sergei <sfccck@rambler.ru>
2022-08-09 22:13:30 +0300

    add IntroArray,intromethod,MethodArray,ArrayLib

---
+++ Lesson_02/Ex02ArrayLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray(int[] collection)      // void убирает метод
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+int [] array = new int[10];           // массив из 10 элементов
+
+FillArray(array);
+PrintArray(array);
+
+// метод заполнения
---
+++ Lesson_02/Ex02ArrayLibrary/Program12.cs
@@ -0,0 +1,31 @@
+void FillArray(int[] collection);
+
+void PrintArray(int[] col);
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int [] array = new int[10];
+
+FillArray(array);                      
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
