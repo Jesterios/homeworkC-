2905db691bc5aa90a015f9ec05cf0a9714c2ad29
Sergei <sfccck@rambler.ru>
2022-08-10 18:34:29 +0300

    Задача 13-ая решена.

--- Lesson_02/Homework00_Lesson02/Program.cs
+++ Lesson_02/Homework00_Lesson02/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+System.Console.Write("Введите число:   ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int a = value % 1000;
+int b = a / 100;
+
+if(b > 0)
+{
+    Console.WriteLine(b);
+}
+else    {
+
+    Console.WriteLine("-");
+}
