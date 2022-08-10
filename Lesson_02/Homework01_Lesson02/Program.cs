3262d299f1b2a96a45ba830642311b1c82bd21fb
Sergei <sfccck@rambler.ru>
2022-08-10 19:49:09 +0300

    10 задача решена

--- Lesson_02/Homework01_Lesson02/Program.cs
+++ Lesson_02/Homework01_Lesson02/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+System.Console.Write("Введите число:   ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int a = value % 100;
+int b = a / 10;
+
+if(b > 0)
+{
+    Console.WriteLine(b);
+}
+else    {
+
+    Console.WriteLine("-");
+}
