e71dfdc4f6b0ac1bb85774572d5ce476fcb74253
Sergei <sfccck@rambler.ru>
2022-09-02 19:38:37 +0300

    Задача 54 сделана

--- Lesson_05/Homework02_Lesson05/Program.cs
+++ Lesson_05/Homework02_Lesson05/Program.cs
@@ -14,35 +14,36 @@ WriteArrayInt(arrayWhole);
 Console.Write($"\nCреднее арифметическое:\n");
 for (int i = 0; i < n; i++)
 {
-  double arithmeticMean = 0;
-  for (int j = 0; j < m; j++)
-  {
-    arithmeticMean += arrayWhole[j, i];
-  }
-  arithmeticMean = Math.Round(arithmeticMean / m, 1);
-  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
-}
-
-int[,] TransformationArrayWhole (double[,] array)
-{
-  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    double arithmeticMean = 0;
+    for (int j = 0; j < m; j++)
     {
-      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+        arithmeticMean += arrayWhole[j, i];
     }
-  }
-  return arrayWhole;
+    arithmeticMean = Math.Round(arithmeticMean / m, 1);
+    Console.WriteLine($"столбца № {i + 1} {arithmeticMean}");
 }
 
-void WriteArrayInt (int[,] arrayWhole){
-for (int i = 0; i < m; i++)
-  {
-      for (int j = 0; j < n; j++)
-      {
-        Console.Write(arrayWhole[i, j] + " ");
-      }
-      Console.WriteLine();
-  }
+int[,] TransformationArrayWhole(double[,] array)
+{
+    int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+        }
+    }
+    return arrayWhole;
+}
+
+void WriteArrayInt(int[,] arrayWhole)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(arrayWhole[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
